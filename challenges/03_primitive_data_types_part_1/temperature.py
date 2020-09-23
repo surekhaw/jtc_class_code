b8a396fcc944e41e85c2a0bd05a65c4b02b7b111
surekha <s.kaniget@gmail.com>
2020-09-22 17:13:59 -0700

    adding temperature.py challenge

---
+++ challenges/03_primitive_data_types_part_1/temperature.py
@@ -0,0 +1,19 @@
+# 1 Convert a temperature of 100 degrees fahrenheit to celsius
+celsius_100 = (100 - 32) * 5/9
+print(celsius_100)
+# The resulting temperature is a float because it is a decimal.
+
+# 2 Convert a temperature of 0 degrees fahrenheit to celsius
+celsius_0 = (0 - 32) * 5/9
+print(celsius_0)
+
+# 3 Convert a temperature of 34.2 degrees fahrenheit to celsius
+# Do this one all in one print statement without saving any variables
+print((34.2 - 32) * 5/9)
+
+# 4 Convert a temperature of 5 degrees celsius to fahrenheit?
+print(5 * 9/5 + 32)
+
+# 5 What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
+print((85.1 - 32) * 5/9) #30.2 degrees celsius is hotter
+
